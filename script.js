15589998b6643d8099f965376ed7bc484c9746b0
SharonChepkoech <chekosharon1738@gmail.com>
2022-08-21 14:32:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+       
+    });
+  
+    $('.navbar .menu li a').click(function(){
+
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    var typed = new Typed(".typing", {
+        strings: ["UX researcher", "UI/UX designer", "Front-end developer", "Back-end developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+   
+  
+});
+
+window.addEventListener("DOMContentLoaded", function () {
+  
+    var form = document.getElementById("contact-form");
+    var status = document.getElementById("status");
+    
+    function success() {
+      form.reset();
+      status.classList.add("success");
+      status.innerHTML = "Thanks!";
+    }
+  
+    function error() {
+      status.classList.add("error");
+      status.innerHTML = "Oops! There was a problem.";
+    }
+
+    
+    form.addEventListener("submit", function (ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
